373bfe83e0cbe5afc5eeefdea2ccaa2bb9aa4b99
Admin <admin@admins-macbook-pro-2.local>
2017-10-10 11:37:20 +0300

    I created the UI, I also added the tableViewCell to the UI and the TableViewCell file.swift

---
+++ VideoCell/myVideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  myVideoTableViewCell.swift
+//  School with Cristina
+//
+//  Created by Admin on 10/10/2017.
+//  Copyright © 2017 home. All rights reserved.
+//
+
+import UIKit
+
+class myVideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
